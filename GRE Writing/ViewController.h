9ab7aacd3c701e3770cd102a375f66cda33579f8
AdySan <adityatannu@gmail.com>
2014-10-05 20:07:39 -0700

    Initial Commit

---
+++ GRE Writing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GRE Writing
+//
+//  Created by Aditya Tannu on 10/5/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GRE Writing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GRE Writing
+//
+//  Created by Aditya Tannu on 10/5/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
